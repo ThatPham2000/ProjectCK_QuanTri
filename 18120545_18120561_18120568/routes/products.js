0e408018f3d14553a02b51b9c0a9fdfda2de321e
ThatPham2000 <thatpham2000@gmail.com>
2020-12-01 20:23:41 +0700

    layout trang product

--- 18120545_18120561_18120568/routes/products.js
+++ 18120545_18120561_18120568/routes/products.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const productController = require('../controllers/product.Controller');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('products', {title: 'Products', subtitle: 'List product'});
-});
+// router.get('/', function(req, res, next) {
+//   res.render('products', {title: 'Products', subtitle: 'List product'});
+// });
+
+router.get('/', productController.index);
 
 module.exports = router;
